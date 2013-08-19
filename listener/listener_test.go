5ccebd008d1567e7c8547b1695f563ac9468639a
Sławosz Sławiński <slawosz@gmail.com>
2013-08-19 20:08:38 +0000

    test for saving listened requests to file wip

--- listener/listener_test.go
+++ listener/listener_test.go
@@ -43,3 +43,35 @@ func TestSendMessage(t *testing.T) {
 		t.Errorf("Original and reveived requests does not match")
 	}
 }
+
+func TestSaveMessageToFile(t *testing.T) {
+	Settings.Verbose = true
+  Settings.FileToReplyPath = "requests.gor"
+
+	received := make(chan int)
+  Run()
+
+  requestBytes = []byte("GET http://localhost:50000/pub/WWW/ HTTP/1.1\nHost: www.w3.org\r\n\r\n")
+  // TODO: implement foo
+  requestReader = foo(requestBytes)
+	request, err = http.ReadRequest(reader)
+
+	go func() {
+    _, err := http.DefaultClient.Do(request)
+		received <- 1
+  }
+
+	select {
+	case <-received:
+	case <-time.After(time.Second):
+		t.Error("Timeout error")
+	}
+
+  file, _ := os.Open("request.gor")
+  buf = make([]byte, 100)
+  n, _ = file.Read(buf)
+
+	if bytes.Compare(buf, requestBytes) != 0 {
+		t.Errorf("Original and reveived requests does not match")
+	}
+}
