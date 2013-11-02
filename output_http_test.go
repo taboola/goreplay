f8a8444b27a594b2eb49d8ffca48d664bcd19a10
Leonid Bugaev <leonsbox@gmail.com>
2013-11-02 16:53:59 +0100

    Add basic benchmakrs

--- output_http_test.go
+++ output_http_test.go
@@ -1,36 +1,43 @@
 package gor
 
 import (
+	"fmt"
 	"io"
+	"net"
 	"net/http"
 	"sync"
 	"testing"
 )
 
+func startHTTP(cb func(*http.Request)) net.Listener {
+	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cb(r)
+	})
+
+	listener, _ := net.Listen("tcp", ":0")
+
+	go http.Serve(listener, handler)
+
+	return listener
+}
+
 func TestHTTPOutput(t *testing.T) {
-	startHTTP := func(addr string, cb func(*http.Request)) {
-		handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			cb(r)
-		})
-
-		go http.ListenAndServe(addr, handler)
-	}
-
 	wg := new(sync.WaitGroup)
 	quit := make(chan int)
 
 	input := NewTestInput()
 
 	headers := HTTPHeaders{HTTPHeader{"User-Agent", "Gor"}}
-	output := NewHTTPOutput("127.0.0.1:50003", headers, "")
 
-	startHTTP("127.0.0.1:50003", func(req *http.Request) {
+	listener := startHTTP(func(req *http.Request) {
 		if req.Header.Get("User-Agent") != "Gor" {
 			t.Error("Wrong header")
 		}
 
 		wg.Done()
 	})
+
+	output := NewHTTPOutput(listener.Addr().String(), headers, "")
 
 	Plugins.Inputs = []io.Reader{input}
 	Plugins.Outputs = []io.Writer{output}
@@ -40,6 +47,37 @@ func TestHTTPOutput(t *testing.T) {
 	for i := 0; i < 100; i++ {
 		wg.Add(2)
 		input.EmitGET()
+		input.EmitPOST()
+	}
+
+	wg.Wait()
+
+	close(quit)
+}
+
+func BenchmarkHTTPOutput(b *testing.B) {
+	wg := new(sync.WaitGroup)
+	quit := make(chan int)
+
+	input := NewTestInput()
+
+	headers := HTTPHeaders{HTTPHeader{"User-Agent", "Gor"}}
+
+	listener := startHTTP(func(req *http.Request) {
+		go wg.Done()
+	})
+
+	output := NewHTTPOutput(listener.Addr().String(), headers, "")
+
+	Plugins.Inputs = []io.Reader{input}
+	Plugins.Outputs = []io.Writer{output}
+
+	go Start(quit)
+
+	fmt.Println(b)
+
+	for i := 0; i < b.N; i++ {
+		wg.Add(1)
 		input.EmitPOST()
 	}
 
