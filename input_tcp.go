3d3cb1dbf5aec8440f075a33f4d4af4d94086fee
Joseph Lawson <joe@joekiller.com>
2014-03-29 18:52:42 -0400

    fixed a reader bug

--- input_tcp.go
+++ input_tcp.go
@@ -63,12 +63,14 @@ func (i *TCPInput) handleConnection(conn net.Conn) {
 	for {
 		buf,err := reader.ReadBytes('¶')
 		new_buf_len := len(buf) - 2
-		new_buf := make([]byte, new_buf_len)
-		copy(new_buf, buf[:new_buf_len])
-		i.data <- new_buf
-		if err != nil {
-			if err != io.EOF {
-				log.Printf("error: %s\n", err)
+		if new_buf_len > 0 {
+			new_buf := make([]byte, new_buf_len)
+			copy(new_buf, buf[:new_buf_len])
+			i.data <- new_buf
+			if err != nil {
+				if err != io.EOF {
+					log.Printf("error: %s\n", err)
+				}
 			}
 		}
 	}
