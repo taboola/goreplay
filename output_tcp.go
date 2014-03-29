cad94ead29e77d5145df8b934e4c7a0d0dc166f5
Joseph Lawson <joe@joekiller.com>
2014-03-28 23:21:47 -0400

    write full message to new array for async buffer

--- output_tcp.go
+++ output_tcp.go
@@ -44,12 +44,14 @@ func (o *TCPOutput) worker() {
 
 	for {
 		conn.Write(<-o.buf)
-		conn.Write([]byte("¶"))
 	}
 }
 
 func (o *TCPOutput) Write(data []byte) (n int, err error) {
-	o.buf <- data
+	new_buf := make([]byte, len(data + 2))
+	append(data, []byte("¶"))
+	copy(new_buf, data)
+	o.buf <- new_buf
 
 	return len(data), nil
 }
