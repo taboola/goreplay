9b0153f45fa9b5b41e0232275607699966198373
Joseph Lawson <joe@joekiller.com>
2014-03-31 11:43:33 -0400

    prevent crash on quits

--- input_tcp.go
+++ input_tcp.go
@@ -62,14 +62,17 @@ func (i *TCPInput) handleConnection(conn net.Conn) {
 
 	for {
 		buf,err := reader.ReadBytes('¶')
-		new_buf_len := len(buf) - 2
-		if new_buf_len > 0 {
-			new_buf := make([]byte, new_buf_len)
-			copy(new_buf, buf[:new_buf_len])
-			i.data <- new_buf
-			if err != nil {
-				if err != io.EOF {
-					log.Printf("error: %s\n", err)
+		buf_len := len(buf)
+		if buf_len > 0 {
+			new_buf_len := len(buf) - 2
+			if new_buf_len > 2 {
+				new_buf := make([]byte, new_buf_len)
+				copy(new_buf, buf[:new_buf_len])
+				i.data <- new_buf
+				if err != nil {
+					if err != io.EOF {
+						log.Printf("error: %s\n", err)
+					}
 				}
 			}
 		}
