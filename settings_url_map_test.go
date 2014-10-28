80572f5f18590ed3c0ff5792774e81b5649d1804
Jan Willies <jan@willies.info>
2014-10-28 14:52:55 +0100

    add regular expression matching to --output-http-rewrite-url

--- settings_url_map_test.go
+++ settings_url_map_test.go
@@ -4,7 +4,7 @@ import (
 	"testing"
 )
 
-func TestUrlRewriteMap(t *testing.T) {
+func TestUrlRewriteMap_1(t *testing.T) {
 	var url string
 
 	rewrites := UrlRewriteMap{}
@@ -15,12 +15,32 @@ func TestUrlRewriteMap(t *testing.T) {
 	}
 
 	url = "/abc"
-
 	if rewrites.Rewrite(url) == url {
 		t.Error("Request url should have been rewritten, wasn't")
 	}
 
 	url = "/wibble"
+	if rewrites.Rewrite(url) != url {
+		t.Error("Request url should not have been rewritten, was")
+	}
+}
+
+func TestUrlRewriteMap_2(t *testing.T) {
+	var url string
+
+	rewrites := UrlRewriteMap{}
+
+	err := rewrites.Set("/abc?\\d{4}5$:/123")
+	if err != nil {
+		t.Error("Should not error on /abc?\\d{4}:/123")
+	}
+
+	url = "/ab12345"
+	if rewrites.Rewrite(url) == url {
+		t.Error("Request url should have been rewritten, wasn't")
+	}
+
+	url = "/ab"
 	if rewrites.Rewrite(url) != url {
 		t.Error("Request url should not have been rewritten, was")
 	}
