bc48e1ad49b768e8b50944e2fb73cbb7c5409d66
Leonid Bugaev <leonsbox@gmail.com>
2015-07-06 18:56:50 +0500

    Fix tests

--- settings_header_hash_filters_test.go
+++ settings_header_hash_filters_test.go
@@ -12,20 +12,8 @@ func TestHTTPHeaderHashFilters(t *testing.T) {
 		t.Error("Should not error on Header1:^$")
 	}
 
-	err = filters.Set("Header2:1/2")
-	if err != nil {
-		t.Error("Should not error on Header2:^:$")
-	}
-
-    // Denominator must be power of 2
-	err = filters.Set("HeaderIrrelevant:1/3")
+	err = filters.Set("Header2:1")
 	if err == nil {
-		t.Error("Should error on HeaderIrrelevant:1/3")
-	}
-
-    // Denominator must be power of 2
-	err = filters.Set("Pow2Denom:1/31")
-	if err == nil {
-		t.Error("Should error on Pow2Denom:1/31")
+		t.Error("Should error on Header2:^:$")
 	}
 }
