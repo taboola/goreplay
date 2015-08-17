69424bf6700fc592157c9e264af5a3575152913c
Leonid Bugaev <leonsbox@gmail.com>
2015-08-17 17:13:55 +0300

    Fix hound

--- examples/middleware/echo_modifier.rb
+++ examples/middleware/echo_modifier.rb
@@ -1,16 +1,15 @@
 #!/usr/bin/env ruby
 # encoding: utf-8
 while data = STDIN.gets
-    next unless data
-    data = data.chomp
+  next unless data
+  data = data.chomp
 
-    decoded = [data].pack("H*")
-    encoded = decoded.unpack("H*").first
+  decoded = [data].pack("H*")
+  encoded = decoded.unpack("H*").first
 
-    STDOUT.puts encoded
+  STDOUT.puts encoded
 
-
-    STDERR.puts "[DEBUG][MIDDLEWARE] Original data: #{data}"
-    STDERR.puts "[DEBUG][MIDDLEWARE] Decoded request: #{decoded}"
-    STDERR.puts "[DEBUG][MIDDLEWARE] Encoded data: #{encoded}"
+  STDERR.puts "[DEBUG][MIDDLEWARE] Original data: #{data}"
+  STDERR.puts "[DEBUG][MIDDLEWARE] Decoded request: #{decoded}"
+  STDERR.puts "[DEBUG][MIDDLEWARE] Encoded data: #{encoded}"
 end
