18dbb7e8ab84541edfcf1ca4474c89069d785f01
Leonid Bugaev <leonsbox@gmail.com>
2016-05-24 20:45:45 +0500

    Disable hanging test

--- http_client_test.go
+++ http_client_test.go
@@ -14,6 +14,7 @@ import (
 	"sync"
 	"testing"
 	"time"
+	"runtime"
 )
 
 func TestHTTPClientURLPort(t *testing.T) {
@@ -202,6 +203,10 @@ func TestHTTPClientHTTPSSend(t *testing.T) {
 }
 
 func TestHTTPClientServerInstantDisconnect(t *testing.T) {
+	if runtime.GOOS == "windows" {
+		return
+	}
+
 	wg := new(sync.WaitGroup)
 
 	GETPayload := []byte("GET / HTTP/1.1\r\n\r\n")
