b01748bbdfa6206f99a4f9ea1c71518f4dd9ac85
Leonid Bugaev <leonsbox@gmail.com>
2016-05-27 20:07:21 +0500

    Update echo.rb

--- examples/middleware/echo.rb
+++ examples/middleware/echo.rb
@@ -1,15 +1,22 @@
 #!/usr/bin/env ruby
 # encoding: utf-8
-while data = STDIN.gets
+while data = STDIN.gets # continiously read line from STDIN
   next unless data
-  data = data.chomp
-
-  decoded = [data].pack("H*")
-  encoded = decoded.unpack("H*").first
-
+  data = data.chomp # remove end of line symbol
+  
+  decoded = [data].pack("H*") # decode base64 encoded request
+  
+  # dedoded value is raw HTTP payload, example:
+  #   
+  #   POST /post HTTP/1.1
+  #   Content-Length: 7
+  #   Host: www.w3.org
+  #
+  #   a=1&b=2"
+  
+  encoded = decoded.unpack("H*").first # encoding back to base64
+  
+  # Emit request back
+  # You can skip this if want to filter out request
   STDOUT.puts encoded
-
-  STDERR.puts "[DEBUG][MIDDLEWARE] Original data: #{data}"
-  STDERR.puts "[DEBUG][MIDDLEWARE] Decoded request: #{decoded}"
-  STDERR.puts "[DEBUG][MIDDLEWARE] Encoded data: #{encoded}"
 end
