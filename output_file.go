264d8cf00e5581a83d1ffee5d62ebf8bdad0260a
Leonid Bugaev <leonsbox@gmail.com>
2019-02-16 09:19:04 +0100

    Add more error handling

--- output_file.go
+++ output_file.go
@@ -246,7 +246,9 @@ func (o *FileOutput) flush() {
 
 		if stat, err := o.file.Stat(); err == nil {
 			o.chunkSize = int(stat.Size())
-		}
+		} else {
+            log.Println("Error accessing file sats", err)
+        }
 	}
 }
 
